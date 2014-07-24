4f4f05937e94978da73bfaf154a54763d7356a99
Free-BSG <Michael.Freeman@oregonstate.edu>
2014-07-24 16:59:27 -0700

    Attempt to center map pin
    I tried to get the map to center on a lat/long and put a pin at that
    point. The point and the pin both ended up in the top lefto corner of the
    map, not the center.

--- build/temp/native/SeaGrant_Proto/sencha-compiler/app/full-page-master-bundle.js
+++ build/temp/native/SeaGrant_Proto/sencha-compiler/app/full-page-master-bundle.js
@@ -1,2 +1,6 @@
 // @tag full-page
+<<<<<<< HEAD
 // @require /home/jacob/devel/SeaGrant_Proto/app.js
+=======
+// @require c:\Users\michael.freeman\Documents\seagrant\SeaGrant_Proto\app.js
+>>>>>>> Attempt to center map pin
