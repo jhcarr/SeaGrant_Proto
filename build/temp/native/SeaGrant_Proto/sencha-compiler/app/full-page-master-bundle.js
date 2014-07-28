adaa8e010f3d319c6f7e4b8f1266438257e09f5c
Jacob Broderick <schmiddy373@gmail.com>
2014-07-28 13:44:48 -0700

    Build project
    Project has been built since last pull. I am commiting in order to pull/merge
    from another branch.

--- build/temp/native/SeaGrant_Proto/sencha-compiler/app/full-page-master-bundle.js
+++ build/temp/native/SeaGrant_Proto/sencha-compiler/app/full-page-master-bundle.js
@@ -1,6 +1,2 @@
 // @tag full-page
-<<<<<<< HEAD
 // @require /home/jacob/devel/SeaGrant_Proto/app.js
-=======
-// @require c:\Users\michael.freeman\Documents\seagrant\SeaGrant_Proto\app.js
->>>>>>> Attempt to center map pin
